576662276e250690bc43ca16e95cdf7bd9cec9ce
theguy951357 <crispspiceguitar@gmail.com>
2024-02-11 15:15:24 -0500

    adjust picture and resize for mobile.

--- public/scripts/curtains.js
+++ public/scripts/curtains.js
@@ -142,9 +142,20 @@ window.addEventListener("load", () => {
 
     const multiTexturesPlane = new Plane(curtains, planeElements[0], params);
 
+    function resizePic(){
+        if(window.innerWidth > 600){
+            multiTexturesPlane.relativeTranslation.x = 40
+            multiTexturesPlane.scale.x = .6
+        }
+    }
+    window.onresize = resizePic();
+    
+
     multiTexturesPlane.onLoading((texture) => {
         // improve texture rendering on small screens with LINEAR_MIPMAP_NEAREST minFilter
         texture.setMinFilter(curtains.gl.LINEAR_MIPMAP_NEAREST);
+        
+        
     }).onReady(() => {
         // the idea here is to create two additionnal textures
         // the first one will contain our visible image
@@ -213,34 +224,3 @@ window.addEventListener("load", () => {
         multiTexturesPlane.uniforms.transitionTimer.value = slideshowState.transitionTimer;
     });
 });
-
-
-// // wait for everything to be ready
-// window.addEventListener("load", () => {
-// // set up our WebGL context and append the canvas to our wrapper
-// const curtains = new Curtains({
-// container: "canvas"
-// });
-// // get our plane element
-// const planeElement = document.getElementsByClassName("plane")[0];
-// // set our initial parameters (basic uniforms)
-// const params = {
-// vertexShaderID: "plane-vs", // our vertex shader ID
-// fragmentShaderID: "plane-fs", // our fragment shader ID
-// uniforms: {
-// time: {
-// name: "uTime", // uniform name that will be passed to our shaders
-// type: "1f", // this means our uniform is a float
-// value: 0,
-// },
-// },
-// };
-// // create our plane using our curtains object, the HTML element and the parameters
-// const plane = new Plane(curtains, planeElement, params);
-// plane.onRender(() => {
-// // use the onRender method of our plane fired at each requestAnimationFrame call
-// plane.uniforms.time.value++; // update our time uniform value
-// });
-// });
-
-
